fa7e488e7227d890c52b2be14a3b4fc8fc6cf18a
george0830120 <george0830120@gmail.com>
2019-12-15 21:07:01 +0800

    first commit

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { InventoryService } from '../../service/inventory.service'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  private inventoryService;
-  constructor(inventoryService: InventoryService) { 
-      this.inventoryService = inventoryService;
-  }
+
 
   ngOnInit() {
   }
