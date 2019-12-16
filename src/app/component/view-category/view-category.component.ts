33a9e35223adbbe7773d7e94bb00dbe2e487726d
george0830120 <george0830120@gmail.com>
2019-12-16 22:24:36 +0800

    merge

--- src/app/component/view-category/view-category.component.ts
+++ src/app/component/view-category/view-category.component.ts
@@ -12,18 +12,17 @@ import { MenuItem } from 'primeng/api';
 })
 export class ViewCategoryComponent implements OnInit {
 
-  private department: Department[];
+  private departmentSelected: Department[];
   private departments: Department[];
   private categories: Category[];
   private breadcrumbArray: MenuItem[];
-  navigationSubscription;  
+  private navigationSubscription;  
   constructor(private route: ActivatedRoute,
     private router: Router,
     private service: InventoryService) { 
       this.navigationSubscription = this.router.events.subscribe((e: any) => {
         // If it is a NavigationEnd event re-initalise the component
-        if (e instanceof NavigationStart) {
-          console.log(e);
+        if (e instanceof NavigationEnd) {
           this.ngOnInit();
         }
       });
@@ -31,19 +30,18 @@ export class ViewCategoryComponent implements OnInit {
 
   ngOnInit() {
     var currentURL = this.route.url; 
-    var departmentName:string;
+    var departmentName: string;
     console.log(currentURL);
     const subscribe = currentURL.subscribe({
       next: val => departmentName = val[1].path 
     })
-    this.department = this.service.getDepartmentByName(departmentName);
-    this.departments = [];
-    this.service.getDepartments().subscribe(val=>this.departments.push(val))
-    this.department.forEach(val=>this.categories = val.categories);
-    console.log(this.categories);
+    this.departmentSelected = this.service.getDepartmentByName(departmentName);
+    this.departmentSelected.forEach(val=>this.categories = val.categories);
 
+    this.departments = [];
+    this.service.getDepartments().subscribe(val=>this.departments.push(val));
+    console.log(this.categories);
     this.addBreadcrumb(departmentName);
-    
   }
 
   addBreadcrumb(departmentName:string){
