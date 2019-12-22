600d7be97b4222f6c204231dff52f10088c96735
SE2019Group1 <jb010506@gmail.com>
2019-12-23 07:56:01 +0800

    add httpclient leaves and subtrees

--- src/app/service/http-client.service.ts
+++ src/app/service/http-client.service.ts
@@ -79,6 +79,62 @@ export class HttpClientService {
     );
   }
 
+  getSubCategories(categoryID: string) {
+    return this.httpClient.get('http://localhost:3000/hierarchy/categories/' + categoryID + 'subcategories',
+      { headers: new HttpHeaders().set('Content-Type', 'application/json'), observe: 'response', withCredentials:true }
+    );
+  }
+
+  addSubCategory(categoryID: string, data: JSON) {
+    return this.httpClient.post('http://localhost:3000/hierarchy/categories/' + categoryID + 'subcategories', data,
+      { headers: new HttpHeaders().set('Content-Type', 'application/json'), observe: 'response', withCredentials:true }
+    );
+  }
+
+  getAllItems() {
+    return this.httpClient.get('http://localhost:3000/items',
+      { headers: new HttpHeaders().set('Content-Type', 'application/json'), observe: 'response', withCredentials:true }
+    );
+  }
+
+  // generic items
+  getDepartementItems(departmentID: string) {
+    return this.httpClient.get('http://localhost:3000/hierarchy/leaf/' + departmentID,
+      { headers: new HttpHeaders().set('Content-Type', 'application/json'), observe: 'response', withCredentials:true }
+    );
+  }
+
+  getDepartmentSubtrees(departmentID: string) {
+    return this.httpClient.get('http://localhost:3000/hierarchy/subtree/' + departmentID,
+      { headers: new HttpHeaders().set('Content-Type', 'application/json'), observe: 'response', withCredentials:true }
+    );
+  }
+
+  getCategoryItems(departmentID: string, categoryID: string) {
+    return this.httpClient.get('http://localhost:3000/hierarchy/leaf/' + departmentID + '/' + categoryID,
+      { headers: new HttpHeaders().set('Content-Type', 'application/json'), observe: 'response', withCredentials:true }
+    );
+  }
+
+  getCategorySubtrees(departmentID: string, categoryID: string) {
+    return this.httpClient.get('http://localhost:3000/hierarchy/subtree/' + departmentID + '/' + categoryID,
+      { headers: new HttpHeaders().set('Content-Type', 'application/json'), observe: 'response', withCredentials:true }
+    );
+  }
+
+  getSubCategoryItems(departmentID: string, categoryID: string, subCategoryID: string) {
+    return this.httpClient.get('http://localhost:3000/hierarchy/leaf/' + departmentID + '/' + categoryID + '/' + subCategoryID,
+      { headers: new HttpHeaders().set('Content-Type', 'application/json'), observe: 'response', withCredentials:true }
+    );
+  }
+
+  getSubCategorySubtrees(departmentID: string, categoryID: string, subCategoryID: string) {
+    return this.httpClient.get('http://localhost:3000/hierarchy/subtree/' + departmentID + '/' + categoryID + '/' + subCategoryID,
+      { headers: new HttpHeaders().set('Content-Type', 'application/json'), observe: 'response', withCredentials:true }
+    );
+  }
+
+
 
   getSubcategoriesbyCategoryID(categoryID: string) {
     return this.httpClient.get('http://localhost:3000/hierarchy/categories/'+ categoryID+'/subcategories',
