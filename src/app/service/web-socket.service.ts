fe8c46effdf502e2da8d7c4873b66dcc944cfc5e
Tim Hsieh <timhost1233@gmail.com>
2020-04-14 14:55:34 +0800

    add webSocket Service

--- src/app/service/web-socket.service.ts
+++ src/app/service/web-socket.service.ts
@@ -1,58 +1,48 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Observable, Subject } from "rxjs";
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class WebSocketService {
+
   ws: WebSocket;
   observable: Observable<any>;
   subject: Subject<any>;
-  connectionID: number;
+
+  cid: number = null;
 
   constructor() {
     this.ws = new WebSocket("ws://localhost:3000");
     this.observable = this.createObservableSocket();
     this.subject = new Subject<any>();
     this.observable.subscribe(this.subject);
-    this.connectionID = null;
   }
 
-  public createObservableSocket(): Observable<any> {
-    return new Observable(observer => {
-      this.ws.onmessage = event => {
-        console.log("[WebSocketService] Received:", event.data);
-        let jsonObj = JSON.parse(event.data);
-        // establish connection
-        if (
-          jsonObj.hasOwnProperty("notification") &&
-          jsonObj["notification"] === "CONNECTIONREADY"
-        ) {
-          this.connectionID = jsonObj["cid"];
-          console.log("connection ready");
-          this.ws.send("testing");
-        }
-        // after connection
-        else {
-          console.log("gogogo");
-          console.log(jsonObj);
-          observer.next(jsonObj);
-        }
-      };
-      this.ws.onerror = event => {
-        console.log("[WebSocketService] Received Error:", event);
-        observer.error(event);
-      };
-      this.ws.onclose = () => {
-        observer.complete();
-        console.log("[WebSocketService] ws closed");
-      };
-      this.ws.onopen = () => {
-        console.log("[WebSocketService]ws opened");
-        this.ws.send("Hello");
-      } 
-    });
+  private createObservableSocket(): Observable<any> {
+    // console.log("createObservableSocket");
+    return new Observable(
+      observer => {
+        this.ws.onmessage = (event) => {
+          console.log("[WebSocketService] Received:", event.data);
+          var jsonObj = JSON.parse(event.data);
+          if (jsonObj.hasOwnProperty("notification") && jsonObj["notification"] === "CONNECTIONREADY") {
+            this.cid = jsonObj["cid"];
+          } else {
+            observer.next(jsonObj);
+          }
+        };
+        this.ws.onerror = (event) => {
+          console.log("[WebSocketService] Received Error:", event);
+          observer.error(event);
+        };
+        this.ws.onclose = () => {
+          observer.complete();
+          console.log("[WebSocketService] ws closed");
+        };
+        this.ws.onopen = () => console.log("[WebSocketService]ws opened");
+      }
+    );
   }
 
   getSubject(): Subject<any> {
@@ -60,9 +50,13 @@ export class WebSocketService {
   }
 
   sendMessage(message: any) {
-    if (this.connectionID) {
-      this.ws.send(message);
-      console.log("[WebSocketService] Sent:", message);
+    if (this.cid == null) {
+      console.log("[WebSocketService] Socket not connected, wait for 1 sec.");
+      setTimeout(() => this.sendMessage(message), 1000);
+      return;
     }
+
+    this.ws.send(message);
+    console.log("[WebSocketService] Sent:", message);
   }
 }
